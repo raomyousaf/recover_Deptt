776a54088b03b7a3915b93fd75117749261cffa8
raomyousaf <raomyousaf@yahoo.com>
2026-07-23 15:24:58 +0500

    email detias to send email to  clients

---
+++ config.py
@@ -0,0 +1,32 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "Abc@123456",
+    "database": "invoice_automation"
+}
+
+UPLOAD_FOLDER = "uploads"
+OUTPUT_FOLDER = "generated_invoices"
+
+# SMTP_CONFIG = {
+#     "host": "smtp.gmail.com",
+#     "port": 587,
+#     "email": "your_email@gmail.com",
+#     "password": "your_app_password"
+# }
+
+
+
+# SMTP_CONFIG = {
+#     "smtp_server": "mail.sospakistan.net",
+#     "smtp_port": 465,
+#     "sender": "digitalsupport@sospakistan.net",
+#     "password": "l#6N(748[Rc#"  # Gmail: use App Password
+# }
+SMTP_CONFIG = {
+    "sender": "callcenter@sospakistan.com",
+    "smtp_port": 465,
+    "password": "jeak luur hpyc ijcw",
+    "smtp_server": "smtp.gmail.com",
+    
+  }
